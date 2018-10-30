2e4439f5cadad866b2ba0cbc8cd0a9361795e165
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2018-10-30 16:09:20 +0100

    adds login screen & refactors registration/login button -> BaseButton

---
+++ src/Components/Buttons/BaseButton/index.js
@@ -0,0 +1,40 @@
+import React,{Component} from 'react';
+import { StyleSheet, TouchableOpacity, Text } from 'react-native';
+import PropTypes from "prop-types";
+import {GREY, PINK} from "../../../colors/colors";
+
+export default class BaseButton extends Component {
+
+  static propTypes = {
+    text: PropTypes.string.isRequired
+  };
+
+
+  render(){
+    return (
+      <TouchableOpacity style={styles.button} {...this.props}>
+        <Text style={[styles.text, this.props.disabled && {opacity: 0.2}]}>{this.props.text}</Text>
+      </TouchableOpacity>
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+  button: {
+    alignSelf: "stretch",
+    marginHorizontal: 30,
+    borderRadius: 4,
+    borderWidth: 2,
+    height: 50,
+    borderColor: PINK.MAIN,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  text: {
+    color: PINK.MAIN,
+    textAlign: "center",
+    fontSize: 20,
+  },
+});
--- src/Screens/LoginScreen/index.js
+++ src/Screens/LoginScreen/index.js
@@ -3,14 +3,12 @@ import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
 import InputField from "../../Components/InputFields/InputField";
 import {validateEmail, validateName, validatePassword} from "../../validationUtil";
 import {GREY, PINK} from "../../colors/colors";
-import BaseButton from "../../Components/Buttons/BaseButton";
 
-export default class RegistrationScreen extends Component {
+export default class LoginScreen extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      isNameValid: false,
       isEmailValid: false,
       isPasswordValid: false,
     }
@@ -36,26 +34,11 @@ export default class RegistrationScreen extends Component {
                       this.setState({isPasswordValid: isValid});
                       return isValid;
                     }}/>
-        <InputField label="Name"
-                    placeholder="e.g. John Doe"
-                    errorText="Name must consist of at least 6 letters"
-                    validate={(name)=>{
-                      const isValid = validateName(name);
-                      this.setState({isNameValid: isValid});
-                      return isValid;
-                    }}/>
+
         <BaseButton
           disabled={!this.state.isEmailValid || !this.state.isNameValid || !this.state.isPasswordValid}
           onPress={()=>{console.log("button pressed")}}
-          text="Register"/>
-
-
-        <TouchableOpacity
-          style={styles.loginBtn}
-          onPress={()=>{console.log("button pressed")}}>
-          <Text
-            style={styles.loginText}>Login</Text>
-        </TouchableOpacity>
+          text="Login"/>
 
       </View>
     );
@@ -68,12 +51,4 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "center",
   },
-  loginBtn: {
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  loginText: {
-    fontSize: 16,
-    textDecorationLine: "underline"
-  }
 });
