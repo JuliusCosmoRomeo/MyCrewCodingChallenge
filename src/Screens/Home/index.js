0661b1739947f58a199483a1f3a7bfa6976c83bc
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2018-11-01 01:41:42 +0100

    adds unicorn animation

--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -1,11 +1,20 @@
 import React,{Component} from 'react';
-import { StyleSheet, View, TouchableOpacity, AsyncStorage, Text, Image } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, AsyncStorage, Dimensions, Text, Image, Animated } from 'react-native';
 import BaseButton from "../../Components/Buttons/BaseButton";
 import {NAME, PASSWORD, EMAIL } from "../../Constants/constants";
 import PropTypes from "prop-types";
+import AudioPlayer from 'react-native-play-audio';
+
+const { width } = Dimensions.get("window");
+
+const url = 'blob:https://www.dropbox.com/7a26d338-0484-4fc3-8263-b91b041d13b3';
+
+let animationValue = new Animated.Value(0);
+const animationDuration = 3000;
 
 export default class Home extends Component {
 
+  _currentAnimValue = 0;
   constructor(props){
     super(props);
     this.state = {
@@ -15,7 +24,9 @@ export default class Home extends Component {
     this.logout = this.logout.bind(this);
     this.playSong = this.playSong.bind(this);
     this.stopSong = this.stopSong.bind(this);
+    animationValue.addListener(({value}) => this._currentAnimValue = value);
   }
+
 
   static propTypes = {
     name: PropTypes.string.isRequired
@@ -28,13 +39,60 @@ export default class Home extends Component {
   }
 
   playSong(){
+
+    if (!this.state.isPlaying){
+      console.log("Preparing Audio player");
+      AudioPlayer.prepare(url, () => {
+        console.log("Audio player prepared, playing now...");
+        AudioPlayer.play();
+
+        AudioPlayer.getDuration((duration) => {
+          console.log(duration);
+        });
+        setInterval(() => {
+          AudioPlayer.getCurrentTime((currentTime) => {
+            console.log(currentTime);
+          });
+        }, 1000);
+      });
+      const duration = (this._currentAnimValue/width) * animationDuration;
+      this.runAnimation(duration);
+    } else {
+      this.pauseSong();
+    }
     this.setState({
       isPlaying: !this.state.isPlaying
     })
+
   }
 
   stopSong(){
+    this.pauseSong();
+    animationValue.setValue(0);
+    this.setState({
+      isPlaying: false
+    })
+  }
 
+  pauseSong(){
+    Animated.timing(
+      animationValue
+    ).stop();
+
+  }
+
+  runAnimation(duration) {
+    Animated.timing(animationValue, {
+      toValue: -(width/2+20),
+      duration,
+      useNativeDriver: true,
+    }).start((o) => {
+      if(o.finished) {
+        animationValue.setValue(width/2+20);
+        console.log("finished")
+        this.runAnimation(animationDuration);
+      }
+    });
   }
 
   render() {
@@ -49,7 +107,13 @@ export default class Home extends Component {
           <TouchableOpacity onPress={this.stopSong}>
             <Image source={require("../../../assets/stopButton.png")}/>
           </TouchableOpacity>
+
         </View>
+        <Animated.View style={styles.animationContainer}>
+          <Text>{"\u{1F984}"}</Text>
+          <Text>{"\u{1F984}"}</Text>
+          <Text>{"\u{1F984}"}</Text>
+        </Animated.View>
         <BaseButton
           onPress={this.logout}
           text="Logout"/>
@@ -78,5 +142,13 @@ const styles = StyleSheet.create({
   },
   logoutBtn: {
     alignSelf: "flex-end"
+  },
+  animationContainer: {
+    flexDirection: "row",
+    alignSelf: "center",
+    transform:[
+      { translateX: animationValue }
+    ]
+
   }
 });
